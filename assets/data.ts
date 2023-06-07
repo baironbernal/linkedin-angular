9da21ddd440a4d6ffad96a8b627f04053185d7ef
Bairon Bernal <bairon.bernal@one-click.com.co>
2023-06-07 09:45:48 -0500

    Auto-generated commit [ci skip]

---
+++ assets/data.ts
@@ -0,0 +1,33 @@
+export const informacion = {
+  "comments": [
+    {
+      "id": 1,
+      "content": "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+      "createdAt": "1 month ago",
+      "score": 12,
+      "user": {
+        "image": { 
+          "png": "assets/images/avatars/image-amyrobson.png",
+          "webp": "assets/images/avatars/image-amyrobson.webp"
+        },
+        "username": "amyrobson"
+      },
+      "replies": [
+        {
+          "id": 8,
+          "content": "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+          "createdAt": "2 days ago",
+          "score": 2,
+          "replyingTo": "ramsesmiron",
+          "user": {
+            "image": { 
+              "png": "assets/images/avatars/image-juliusomo.png",
+              "webp": "assets/images/avatars/image-juliusomo.webp"
+            },
+            "username": "juliusomo"
+          }
+        }
+      ]
+    },
+  ]
+}
